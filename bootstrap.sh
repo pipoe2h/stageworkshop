6694d5160547df5b1fb464a7809246bd2f42fd7c
Jose Gomez <pipoe2h@gmail.com>
2019-02-26 13:10:16 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@
 # curl --remote-name --location https://raw.githubusercontent.com/mlavi/stageworkshop/master/bootstrap.sh && SOURCE=${_} sh ${_##*/}
 
 if [[ -z ${SOURCE} ]]; then
-  ORGANIZATION=pipoe2h
+  ORGANIZATION=nutanixworkshops
     REPOSITORY=stageworkshop
         BRANCH=master
 else
